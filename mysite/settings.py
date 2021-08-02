9f303f780d2ec9eed21f0fac169cc5bdf2b0ef52
jasdepw <jasdepw@gmail.com>
2021-08-02 19:40:47 +0900

    Write first Django app, part 2
     - database setup
     - create models
     - create an admin user

--- mysite/settings.py
+++ mysite/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,9 +119,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-KR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
